28ede0417748848b2e3c6c062b62ccff49e962f5
wcyat <wcyat@wcyat.me>
2024-06-08 22:39:02 +0800

    remove mermaid

--- app/hooks/use-markdown-processor.tsx
+++ app/hooks/use-markdown-processor.tsx
@@ -1,27 +1,20 @@
 // src/hooks/use-markdown-processor.tsx
 
-// Custom Dialog component rendered with Radix.
-import { Dialog } from "@/components/ui/dialog";
 // highlight.js syntax highlighting theme for the code blocks.
 import "highlight.js/styles/base16/green-screen.css";
 // Import all of the necessary packages.
-import mermaid from "mermaid";
-import { createElement, useEffect, useMemo, useState } from "react";
+import { createElement, useMemo } from "react";
 import rehypeHighlight from "rehype-highlight";
-import rehypeMathjax from 'rehype-mathjax'
+import rehypeMathjax from "rehype-mathjax";
 import rehypeReact from "rehype-react";
 import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkMath from "remark-math";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
-import * as prod from 'react/jsx-runtime'
+import * as prod from "react/jsx-runtime";
 
 export const useMarkdownProcessor = (content: string) => {
-  useEffect(() => {
-    mermaid.initialize({ startOnLoad: false, theme: "forest" });
-  }, []);
-
   return useMemo(() => {
     return (
       unified()
@@ -40,7 +33,8 @@ export const useMarkdownProcessor = (content: string) => {
         .use(rehypeReact, {
           createElement,
           Fragment: prod.Fragment,
-          jsx: prod.jsx, jsxs: prod.jsxs,
+          jsx: prod.jsx,
+          jsxs: prod.jsxs,
           components: {
             a: ({ href, children }: JSX.IntrinsicElements["a"]) => (
               <a href={href} target="_blank" rel="noreferrer" className="...">
@@ -129,74 +123,12 @@ export const useMarkdownProcessor = (content: string) => {
 
 // A more complex custom component for the `code` element.
 const CodeBlock = ({ children, className }: JSX.IntrinsicElements["code"]) => {
-  // State to display the Mermaid diagram.
-  const [showMermaidPreview, setShowMermaidPreview] = useState(false);
-
   // Highlight.js adds a `className` so this is a hack to detect if the code block
   // is a language block wrapped in a `pre` tag versus an inline `code` tag.
   if (className) {
-    // Determine if it's a mermaid diagram code block.
-    const isMermaid = className.includes("language-mermaid");
-
-    return (
-      <>
-        <code className={className}>{children}</code>
-        {/* If the code block is a Mermaid diagram, display additional UI to allow rendering it. */}
-        <div className="...">
-          {isMermaid ? (
-            <>
-              <button
-                type="button"
-                className="..."
-                onClick={() => {
-                  setShowMermaidPreview(true);
-                }}
-              >
-                Open Mermaid preview
-              </button>
-              <Dialog
-                open={showMermaidPreview}
-                onOpenChange={setShowMermaidPreview}
-              >
-                <Mermaid content={children?.toString() ?? ""} />
-              </Dialog>
-            </>
-          ) : null}
-        </div>
-      </>
-    );
+    return <code className={className}>{children}</code>;
   }
 
   // Handle an inline `code` tag.
   return <code className="...">{children}</code>;
-};
-
-// A custom component to render a Mermaid diagram given the string.
-const Mermaid = ({ content }: { content: string }) => {
-  const [diagram, setDiagram] = useState<string | boolean>(true);
-
-  useEffect(() => {
-    const render = async () => {
-      // Generate a random ID for Mermaid to use.
-      const id = `mermaid-svg-${Math.round(Math.random() * 10000000)}`;
-
-      // Confirm the diagram is valid before rendering since it could be invalid
-      // while streaming, or if the LLM "hallucinates" an invalid diagram.
-      if (await mermaid.parse(content, { suppressErrors: true })) {
-        const { svg } = await mermaid.render(id, content);
-        setDiagram(svg);
-      } else {
-        setDiagram(false);
-      }
-    };
-    render();
-  }, [content]);
-
-  if (diagram === true) {
-    return <p className="...">Rendering diagram...</p>;
-  } else if (diagram === false) {
-    return <p className="...">Unable to render this diagram.</p>;
-  } else {
-    return <div dangerouslySetInnerHTML={{ __html: diagram ?? "" }} />;
-  }
 };
